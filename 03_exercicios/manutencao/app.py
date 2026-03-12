da3f919fa2d705885cfd4e755dc23e188aeedd5b
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2026-03-12 20:21:14 -0300

    atualizando

---
+++ 03_exercicios/manutencao/app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import psycopg
+from flask import render_template
+from flask import abort, redirect, url_for
+from flask import request
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/adicionar_usuario", methods=['POST'])
+def adicionar_usuario():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+    with psycopg.connect("dbname=manutencao host=localhost port=5432 user=postgres password=postgres") as conn:
+        with conn.cursor() as cur:
+            cur.execute("CALL cadastra_usuario(%s, %s, %s)", [nome, email, senha])
+    return redirect(url_for('index'))
+
+
+@app.route("/tela_adicionar")
+def tela_adicionar():
+    return render_template('tela_adicionar.html')
+
+
+@app.route("/")
+def index():
+    with psycopg.connect("dbname=manutencao host=localhost port=5432 user=postgres password=postgres") as conn:
+        with conn.cursor() as cur:
+            # cur.execute("select * from busque_usuario_por_email(%s);", [email])
+            # print(cur.fetchone())
+            cur.execute("select * from usuario;")
+            return render_template('index.html', vetUsuario=cur.fetchall())
+    # return "<p>Hello, World!</p>"
